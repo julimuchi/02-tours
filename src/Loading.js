fecc02df08480d710b21ebfb5afd7e16482c7f06
Julián Mutchinick <julimuchi@gmail.com>
2021-06-13 12:12:14 -0300

    First commit

---
+++ src/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Loading.css'
+const Loading = () => {
+  return (
+    <div className="loader">
+      <h1>loading...</h1>
+    </div>
+  );
+};
+
+export default Loading;
